8ae85895a917387d9c4458e2585a8f289f77aa9d
Yoshinori Tahara <read.eval.print@gmail.com>
2009-07-03 23:44:55 +0900

    a is not bound

--- cl-vh.lisp
+++ cl-vh.lisp
@@ -4,36 +4,50 @@
 
 (defvar *vh-text-style* (make-text-style :fix nil nil))
 
-(defclass vh-pane (drei-pane esa-pane-mixin)
-  ()
-  (:metaclass esa-utils:modual-class))
-
 (defclass vh-buffer (drei-buffer)
   ((external-format :initform *default-external-format*
                     :accessor external-format)))
 
+(defclass vh-pane (drei-pane esa-pane-mixin)
+  ()
+  (:metaclass esa-utils:modual-class)
+  (:default-initargs
+      :view (make-instance 'textual-drei-syntax-view
+                           :buffer (make-instance 'vh-buffer))))
+
 (defclass vh-minibuffer-pane (minibuffer-pane)
   ())
 
-(define-application-frame vh-frame (esa-frame-mixin standard-application-frame)
+(make-command-table 'input-table :errorp nil)
+
+(make-command-table 'edit-table :errorp  nil)
+
+(define-application-frame vh (esa-frame-mixin standard-application-frame)
   ()
   (:menu-bar t)
   (:panes
-   (vh-window
+   (window
     (let* ((*esa-instance* *application-frame*)
            (vh-pane (make-pane 'vh-pane :active t)))
+      (setf (windows *application-frame*) (list vh-pane))
       (vertically () (scrolling () vh-pane ))))
    (minibuffer (make-pane 'vh-minibuffer-pane))
-   (interactor :interactor))
+   ;;(interactor :interactor)
+   )
   (:layouts
    (defalut
        (vertically (:scroll-bars nil)
-         vh-window
+         window
          minibuffer
-         interactor))))
+         ;;interactor
+         )))
+  (:top-level (esa-top-level)))
 
-(define-vh-frame-command (com-quit :name t :menu t :keystroke (#\q :meta)) ()
+(define-vh-command (com-quit :name t :menu t :keystroke (#\q :meta)) ()
   (frame-exit *application-frame*))
 
+(defmethod drei-instance-of ((frame vh))
+  (esa-current-window frame))
+
 #+nil
-(run-frame-top-level (make-instance 'vh-frame))
+(run-frame-top-level (make-instance 'vh))
